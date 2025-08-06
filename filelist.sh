886e4b3205dc49669bb1bf311b1636fc7d86f358
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2025-08-06 13:50:39 +0000

    Update filelist.sh

--- filelist.sh
+++ filelist.sh
@@ -31,7 +31,7 @@ file2vivado(){
     then
       line=${line#*+incdir+}
       if [[ -n $line ]]; then
-	      echo "set include_dirs [concat $line \$include_dirs]" >> $2
+        echo "set include_dirs [concat $line \$include_dirs]" >> $2
       fi
     elif [[ "$line" =~ ^\/\/ ]] 
     then
@@ -39,6 +39,54 @@ file2vivado(){
     else
       if [[ -n $line ]]; then
         echo "add_files $line" >> $2
+      fi
+    fi
+  done < <(cat $parsed_path)
+}
+file2quartus(){
+  eval parsed_path=$1
+  echo "reading $parsed_path"
+  while IFS= read -r line; do
+    if [[ "$line" =~ ^-f ]] 
+    then
+      line=${line#*-f}
+      file2quartus $line $2 
+    elif [[ "$line" =~ ^\+incdir\+ ]] 
+    then
+      line=${line#*+incdir+}
+      if [[ -n $line ]]; then
+        echo "set_global_assignment -name SEARCH_PATH $line" >> $2
+      fi
+    elif [[ "$line" =~ ^\/\/ ]] 
+    then
+      line=''
+    else
+      if [[ -n $line ]]; then
+        echo "set_global_assignment -name VERILOG_FILE $line" >> $2
+      fi
+    fi
+  done < <(cat $parsed_path)
+}
+file2yosys(){
+  eval parsed_path=$1
+  echo "reading $parsed_path"
+  while IFS= read -r line; do
+    if [[ "$line" =~ ^-f ]] 
+    then
+      line=${line#*-f}
+      file2yosys $line $2 
+    elif [[ "$line" =~ ^\+incdir\+ ]] 
+    then
+      line=${line#*+incdir+}
+      if [[ -n $line ]]; then
+        echo "verilog_defaults -add -I$line" >> $2
+      fi
+    elif [[ "$line" =~ ^\/\/ ]] 
+    then
+      line=''
+    else
+      if [[ -n $line ]]; then
+        echo "read_verilog $line" >> $2
       fi
     fi
   done < <(cat $parsed_path)
@@ -53,8 +101,18 @@ elif [[ "$#" -eq 3 && "${1,,}" == "vivado" ]]; then
   echo 'set include_dirs [list]' > $3
   file2vivado $2 $3
   sed -i '/^[[:space:]]*$/d' $3
+elif [[ "$#" -eq 3 && "${1,,}" == "quartus" ]]; then
+  echo '' > $3
+  file2quartus $2 $3
+  sed -i '/^[[:space:]]*$/d' $3
+elif [[ "$#" -eq 3 && "${1,,}" == "yosys" ]]; then
+  echo '' > $3
+  file2yosys $2 $3
+  sed -i '/^[[:space:]]*$/d' $3
 else
   echo "filelist  "
-  echo "    split   <input file list>   <output file list>          <output include list>  "
-  echo "    vivado  <input file list>   <output vivado read tcl>                           "
+  echo "    split    <input file list>   <output file list>            <output include list>  "
+  echo "    vivado   <input file list>   <output vivado read tcl>                             "
+  echo "    quartus  <input file list>   <output quartus read tcl>                            "
+  echo "    yosys    <input file list>   <output yosys read script>                           "
 fi
