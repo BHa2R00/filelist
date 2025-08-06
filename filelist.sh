a86057416dde1f74c1a585a112279d11a4a0005f
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2025-08-06 13:29:33 +0000

    Update filelist.sh

--- filelist.sh
+++ filelist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-filelist(){
+split(){
   eval parsed_path=$1
   echo "reading $parsed_path"
   while IFS= read -r line; do
     if [[ "$line" =~ ^-f ]] 
     then
       line=${line#*-f}
-      filelist $line $2 $3
+      split $line $2 $3
     elif [[ "$line" =~ ^\+incdir\+ ]] 
     then
       line=${line#*+incdir+}
@@ -19,12 +19,42 @@ filelist(){
     fi
   done < <(cat $parsed_path)
 }
-if [[ "$#" -eq 3 ]]; then
-  echo '' > $2
+file2vivado(){
+  eval parsed_path=$1
+  echo "reading $parsed_path"
+  while IFS= read -r line; do
+    if [[ "$line" =~ ^-f ]] 
+    then
+      line=${line#*-f}
+      file2vivado $line $2 
+    elif [[ "$line" =~ ^\+incdir\+ ]] 
+    then
+      line=${line#*+incdir+}
+      if [[ -n $line ]]; then
+	      echo "set include_dirs [concat $line \$include_dirs]" >> $2
+      fi
+    elif [[ "$line" =~ ^\/\/ ]] 
+    then
+      line=''
+    else
+      if [[ -n $line ]]; then
+        echo "add_files $line" >> $2
+      fi
+    fi
+  done < <(cat $parsed_path)
+}
+if [[ "$#" -eq 4 && "${1,,}" == "split" ]]; then
   echo '' > $3
-  filelist $1 $2 $3
-  sed -i '/^[[:space:]]*$/d' $2
+  echo '' > $4
+  split $2 $3 $4
+  sed -i '/^[[:space:]]*$/d' $3
+  sed -i '/^[[:space:]]*$/d' $4
+elif [[ "$#" -eq 3 && "${1,,}" == "vivado" ]]; then
+  echo 'set include_dirs [list]' > $3
+  file2vivado $2 $3
   sed -i '/^[[:space:]]*$/d' $3
 else
-  echo "filelist <input file list> <output file list> <output include list>"
+  echo "filelist  "
+  echo "    split   <input file list>   <output file list>          <output include list>  "
+  echo "    vivado  <input file list>   <output vivado read tcl>                           "
 fi
